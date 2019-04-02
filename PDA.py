78d1bcb0d3819868b5d35ac03a11975445274605
Mohit Singh <mohitwildbeast@gmail.com>
2019-04-02 09:34:36 +0530

    Improved output format and added time.sleep() for delay effect

--- PDA.py
+++ PDA.py
@@ -1,4 +1,5 @@
 from FileHandler import FileHandler
+import time
 
 class PDA:
     def __init__(self):
@@ -16,21 +17,22 @@ class PDA:
         currentStackSymbol = initStackSymbol
         currentState = initialState
 
+        print('State\tInput\tStack\tMove')
+        print('{}\t {}\t {}\t ({}, {})'.format(currentState, '_', 'Z', currentStackSymbol, self.stack))
         for char in inputString:
-            print('Current TOS', currentStackSymbol)
+            #print('Current TOS', currentStackSymbol)
             for production in productions:
                 if ((production[0] == currentState) and (production[1] == char) and (production[2] == currentStackSymbol)):
                     currentState = production[3]
                     if(len(production[4]) > 1):
                         self.stack.append(char)
                     elif ((production[4] == 'e') and (len(self.stack) != 0)):
-                        print('Entered here')
                         self.stack.pop()
                         break
+            previousStackSymbol = currentStackSymbol
             currentStackSymbol = self.stack[len(self.stack)-1]
-            print('Moved: ({}, {})'.format(currentState, self.stack))
-
-            
+            print('{}\t {}\t {}\t ({}, {})'.format(currentState, char, previousStackSymbol, currentStackSymbol, self.stack))
+            time.sleep(2)
         if(len(self.stack) == 1 or currentState in finalStates):
             print('String accepted by PDA.')
         else:
@@ -41,10 +43,27 @@ def main():
     pda = PDA()
     automataFilePath = input('Enter the automata file path: ')
     lines = fh.readFile(automataFilePath)
+    print('Reading Automata File')
+    time.sleep(2)
+    print('Automata File Successfully Read')
+    time.sleep(2)
     inputString = input('Enter input String: ')
     inputString = inputString.rstrip()
+    print('Loading Details from Automata File: ')
+    time.sleep(3)
     parsedLines = fh.parseFile(lines)
-    print(parsedLines)
+    print('States: ', parsedLines['states'])
+    print('Input Symbols: ', parsedLines['input_symbols'])
+    print('Stack Symbols: ', parsedLines['stack_symbols'])
+    print('Initial State: ', parsedLines['initial_state'])
+    print('Initial Stack Symbol: ', parsedLines['initial_stack'])
+    print('Final States: ', parsedLines['final_states'])
+    print('Productions List:')
+    for production in parsedLines['productions']:
+        print('\t', production)
+    time.sleep(2)
+    print('Details loaded')
+    print('Computing the Transition Table:')
     pda.compute(inputString, parsedLines)
 
 if __name__ == '__main__':
